d3fb10158cd6ac66f7c3120fe7a62775224f06d7
Cullen Jennings <fluffy@iii.ca>
2016-01-02 09:41:08 -0700

    output XML json

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # senmlCat
 Tool to convert senml between formats and act as gateway server to other services
 
+# usage
+
+## convert json senml to xml
+
+./senmlCat -json -i data.json > data.xml
+
+
--- senmlCat.go
+++ senmlCat.go
@@ -3,31 +3,35 @@ package main
 import (
 	"fmt"
 	"encoding/json"
+	"encoding/xml"
 	"flag"
 	"os"
 	"io/ioutil"
 )
 
 type SenMLRecord struct {
-	BaseName string `json:"bn,omitempty"  `
-	BaseTime int `json:"bt,omitempty" `
-	BaseUnit string `json:"bu,omitempty" `
-	Version int `json:"ver,omitempty" `
-	Name string `json:"n,omitempty" `
-	Unit string `json:"u,omitempty" `
-	Time int `json:"t,omitempty" `
-	UpdateTime int `json:"ut,omitempty" `
+	XMLName *bool `json:"_,omitempty" xml:"senml"`
+	BaseName string `json:"bn,omitempty"  xml:"bn,attr,omitempty"`
+	BaseTime int `json:"bt,omitempty"  xml:"bt,attr,omitempty"`
+	BaseUnit string `json:"bu,omitempty"  xml:"bu,attr,omitempty"`
+	Version int `json:"ver,omitempty"  xml:"ver,attr,omitempty"`
+	Name string `json:"n,omitempty"  xml:"n,attr,omitempty"`
+	Unit string `json:"u,omitempty"  xml:"u,attr,omitempty"`
+	Time int `json:"t,omitempty"  xml:"t,attr,omitempty"`
+	UpdateTime int `json:"ut,omitempty"  xml:"ut,attr,omitempty"`
 
-	Value *float64 `json:"v,omitempty" `
-	StringValue string `json:"sv,omitempty" `
-	BoolValue *bool `json:"bv,omitempty" `
+	Value *float64 `json:"v,omitempty"  xml:"v,attr,omitempty"`
+	StringValue string `json:"sv,omitempty"  xml:"sv,attr,omitempty"`
+	BoolValue *bool `json:"bv,omitempty"  xml:"bv,attr,omitempty"`
 	
-	Sum *float64 `json:"s,omitempty" `
+	Sum *float64 `json:"s,omitempty"  xml:"sum,,attr,omitempty"`
 }  
 
 type SenMLStream []SenMLRecord;
 
 type SenML struct {
+	XMLName *bool `json:"_,omitempty" xml:"senmls"`
+	Xmlns string `json:"_,omitempty" xml:"xmlns,attr"`
 	SenMLStream
 }
 
@@ -36,6 +40,8 @@ func main() {
 	var err error;
 
 	doIndentPtr := flag.Bool("i", false, "indent output")
+	doJSONPtr := flag.Bool("json", false, "output JSON formatted SENML ")
+	doXMLPtr := flag.Bool("xml", false, "output XML formatted SENML ")
 	flag.Parse()
 
 	// load the input JSON 
@@ -54,15 +60,35 @@ func main() {
 		os.Exit( 1 )
 	}
 
-	var d []byte;
-	if ( *doIndentPtr ) {
-		d,err = json.MarshalIndent( s, "", "  " )
-	} else {
-		d,err = json.Marshal( s )
+	// ouput JSON version 
+	if ( *doJSONPtr ) {
+		var d []byte;
+		if ( *doIndentPtr ) {
+			d,err = json.MarshalIndent( s, "", "  " )
+		} else {
+			d,err = json.Marshal( s )
+		}
+		if err != nil {
+			fmt.Printf("error encoding json %v\n",err)
+			os.Exit( 1 )
+		}
+		fmt.Printf("%s\n", d)
 	}
-	if err != nil {
-		fmt.Printf("error encoding json %v\n",err)
-		os.Exit( 1 )
+
+	// out put a XML version 
+	if ( *doXMLPtr ) {
+		var d []byte;
+		var sxml SenML = SenML{ nil, "urn:ietf:params:xml:ns:senml", s }
+		
+		if ( *doIndentPtr ) {
+			d,err = xml.MarshalIndent( sxml, "", "  " )
+		} else {
+			d,err = xml.Marshal( sxml )
+		}
+		if err != nil {
+			fmt.Printf("error encoding xml %v\n",err);	
+		}
+		fmt.Printf("%s\n", d)
 	}
-	fmt.Printf("%s\n", d)
+	
 }
